fd2b95bf7d8ec20e695b2a5d0bca7d0f6cf56ba4
Liza Morrison <lizammorrison@gmail.com>
2024-11-12 19:12:50 -0800

    add type animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 
+import { TypeAnimation } from 'react-type-animation';
+
 function App() {
   return (
     <div className="w-full h-full m-0">
       <header className="bg-dolomites bg-cover bg-[55%] h-screen">
-        <p className="text-5xl font-bold leading-5 text-sky-950 fixed top-[15%] left-[20%]">
-          Hi, I'm Liza.
-        </p>
+        <div className="text-2xl font-bold leading-5 text-sky-950 fixed top-[15%] left-[20%]">
+          <TypeAnimation
+            sequence={[
+              "Hi, I'm Liza.",
+              1000,
+              "Hi, I'm an artist.",
+              1000,
+              "Hi, I'm a software engineer.",
+              1000,
+              "Hi, I'm a scientist.",
+              1000,
+              "Hi, I'm an adventurist.",
+              1000,
+              "Hi, I'm Liza.",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: "2em", display: "inline-block" }}
+            cursor={false}
+          />
+        </div>
       </header>
     </div>
   );
